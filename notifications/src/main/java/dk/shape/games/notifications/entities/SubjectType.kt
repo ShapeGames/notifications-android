fbe74563d3f1ff69ce1c21e31ade6e8d1efc9dda
Benoit Hebrard <benoit@shape.dk>
2020-06-09 17:04:46 +0200

    add serializable

--- notifications/src/main/java/dk/shape/games/notifications/entities/SubjectType.kt
+++ notifications/src/main/java/dk/shape/games/notifications/entities/SubjectType.kt
@@ -1,10 +1,12 @@
 package dk.shape.games.notifications.entities
 
 import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
 
 /**
  * Entity representing all subject types that can be used for stats notifications.
  */
+@Serializable
 enum class SubjectType {
     @SerializedName("events")
     EVENTS,
