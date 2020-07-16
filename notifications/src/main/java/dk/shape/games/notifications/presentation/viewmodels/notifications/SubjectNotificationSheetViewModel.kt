aa72a324eabdb56696767824b89b7e3db482bcf4
Benoit Hebrard <benoit@shape.dk>
2020-07-16 15:07:05 +0200

    added error view model

--- notifications/src/main/java/dk/shape/games/notifications/presentation/viewmodels/notifications/SubjectNotificationSheetViewModel.kt
+++ notifications/src/main/java/dk/shape/games/notifications/presentation/viewmodels/notifications/SubjectNotificationSheetViewModel.kt
@@ -1,10 +1,12 @@
 package dk.shape.games.notifications.presentation.viewmodels.notifications
 
 import dk.shape.games.notifications.presentation.viewmodels.settings.NotificationsToolbarViewModel
+import dk.shape.games.notifications.presentation.viewmodels.state.ErrorMessageViewModel
 
 internal data class SubjectNotificationSheetViewModel(
     val notificationViewModel: NotificationSheetSubjectViewModel,
     val notificationSwitcherViewModel: SubjectNotificationSwitcherViewModel,
+    val errorMessageViewModel: ErrorMessageViewModel,
     private val onBackPressed: () -> Unit
 ) {
     val toolbarViewModel: NotificationsToolbarViewModel =
