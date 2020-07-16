d61c33da95e983753862ad76380bca8b204ea488
Benoit Hebrard <benoit@shape.dk>
2020-07-16 15:15:32 +0200

    add error top all layouts

--- notifications/src/main/java/dk/shape/games/notifications/presentation/viewmodels/settings/NotificationSettingsTypesEventViewModel.kt
+++ notifications/src/main/java/dk/shape/games/notifications/presentation/viewmodels/settings/NotificationSettingsTypesEventViewModel.kt
@@ -1,9 +1,11 @@
 package dk.shape.games.notifications.presentation.viewmodels.settings
 
 import dk.shape.games.notifications.presentation.viewmodels.notifications.NotificationSheetEventViewModel
+import dk.shape.games.notifications.presentation.viewmodels.state.ErrorMessageViewModel
 
 internal data class NotificationSettingsTypesEventViewModel(
     val notificationViewModel: NotificationSheetEventViewModel,
+    val errorMessageViewModel: ErrorMessageViewModel,
     private val onBackPressed: () -> Unit
 ) {
     val toolbarViewModel: NotificationsToolbarViewModel =
--- notifications/src/main/java/dk/shape/games/notifications/presentation/viewmodels/settings/NotificationsSettingsViewModel.kt
+++ notifications/src/main/java/dk/shape/games/notifications/presentation/viewmodels/settings/NotificationsSettingsViewModel.kt
@@ -1,6 +1,9 @@
 package dk.shape.games.notifications.presentation.viewmodels.settings
 
+import dk.shape.games.notifications.presentation.viewmodels.state.ErrorMessageViewModel
+
 data class NotificationsSettingsViewModel(
         val toolbarViewModel: NotificationsToolbarViewModel,
-        val switcherViewModel: NotificationsSettingsSwitcherViewModel
+        val switcherViewModel: NotificationsSettingsSwitcherViewModel,
+        val errorMessageViewModel: ErrorMessageViewModel
 )
