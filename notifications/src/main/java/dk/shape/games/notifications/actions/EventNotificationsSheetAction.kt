707c7f2b8399e16543de15d3fce08377fc05379d
Benoit Hebrard <benoit@shape.dk>
2020-09-08 15:44:13 +0200

    add extension function

--- notifications/src/main/java/dk/shape/games/notifications/actions/EventNotificationsSheetAction.kt
+++ notifications/src/main/java/dk/shape/games/notifications/actions/EventNotificationsSheetAction.kt
@@ -1,8 +1,8 @@
 package dk.shape.games.notifications.actions
 
 import android.os.Parcelable
-import com.google.gson.annotations.SerializedName
-import dk.shape.games.notifications.entities.SubjectType
+import dk.shape.games.notifications.extensions.toEventInfo
+import dk.shape.games.sportsbook.offerings.modules.event.data.Event
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
@@ -11,3 +11,9 @@ data class EventNotificationsSheetAction(
     val eventInfo: EventInfo,
     val groupId: String
 ) : Parcelable
+
+fun Event.toEventNotificationSheetAction() = EventNotificationsSheetAction(
+    eventId = id,
+    groupId = notificationConfigurationId,
+    eventInfo = toEventInfo()
+)
