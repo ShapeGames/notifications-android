5162aa2d8ed8f46d94b4aade30143100d473a42d
Jokubas <jokubas@shape.dk>
2021-01-18 14:39:45 +0200

    made NotificationSettingsActionConfig parcelable

--- notifications/src/main/java/dk/shape/games/notifications/actions/NotificationSettingsActionConfig.kt
+++ notifications/src/main/java/dk/shape/games/notifications/actions/NotificationSettingsActionConfig.kt
@@ -1,17 +1,24 @@
 package dk.shape.games.notifications.actions
 
-sealed class NotificationSettingsActionConfig {
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+sealed class NotificationSettingsActionConfig: Parcelable {
+
+    @Parcelize
     data class IncludeAllEvents(
         val eventIds: List<String>
     ) : NotificationSettingsActionConfig()
 
     @Deprecated("This will not be used with a release of new BetHistory")
+    @Parcelize
     object FilterBetEvents : NotificationSettingsActionConfig()
 
+    @Parcelize
     data class FilterBetSlip(
         val betSlipComponentUUID: String? = null
     ) : NotificationSettingsActionConfig()
 
+    @Parcelize
     object Default: NotificationSettingsActionConfig()
 }
